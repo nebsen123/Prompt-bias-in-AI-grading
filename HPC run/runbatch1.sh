fa55a2ec149ffd0df3f3770482a006eefb157249
nebsen123 <esbenyo@gmail.com>
2026-06-18 12:05:55 +0200

    cleaned up

--- HPC run/runbatch1.sh
+++ HPC run/runbatch1.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-#BSUB -J Batch1
-#BSUB -q gpuv100
-#BSUB -R "rusage[mem=32GB]"
+#BSUB -J Batch1gptoss1GPU3Test
+#BSUB -q gpua100
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "select[gpu80gb]"
 #BSUB -B
 #BSUB -N
 ##BSUB -u esbenyo@gmail.com
-#BSUB -o Output_%J.out
-#BSUB -e Output_%J.err
-#BSUB -W 1:00
+#BSUB -o Output_%JBatch1.out
+#BSUB -e Output_%JBatch1.err
+#BSUB -W 2:00
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
-#BSUB -gpu "num=2:mode=exclusive_process"
+#BSUB -gpu "num=1:mode=exclusive_process"
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
 module load cuda/11.8
  
 # Starts llama.cpp server
-~/dtu/blackhole/03/225731/llama.cpp/build/bin/llama-server \
-    -m ~/dtu/blackhole/03/225731/llama.cpp/qwen3-235b-q4/Qwen3-235B-A22B-Q4_K_M.gguf \
-    --host 127.0.0.1 \
-    --port 11434 &
+/dtu/blackhole/03/225731/llama.cpp/build/bin/llama-server \
+  -m /dtu/blackhole/03/225731/llama.cpp/myModels/gpt-oss-120b/gpt-oss-120b-mxfp4-00001-of-00003.gguf \
+  --split-mode layer\
+  -ngl all \
+  --host 127.0.0.1 \
+  --port 11434 &
+
+LLAMA_PID=$!
+trap 'kill "$LLAMA_PID" 2>/dev/null || true' EXIT
  
 # Waits until the server and model are fully loaded
 echo "Waiting for llama-server to start..."
@@ -32,4 +38,5 @@ echo "Server is up, starting batch jobs..."
 python3 batch1.py > joboutput_batch1_$LSB_JOBID.out 2>&1
  
 # Shuts down the server
-kill %1
+kill "$LLAMA_PID" 2>/dev/null || true
+wait "$LLAMA_PID" 2>/dev/null || true
